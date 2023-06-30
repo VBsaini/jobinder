fa06a3ea86ff4a83cbe352d34bdbdcd00cd650e8
vaibhav-saini-Edifice <vbsaini2004@email.com>
2023-06-30 13:49:06 +0530

    some minor changes

--- middleware/index.js
+++ middleware/index.js
@@ -1,25 +1,25 @@
 var middlewareObj = {};
 
-middlewareObj.isloggedin = function (req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    }
-    req.flash("error", "please log in or sign up to access that page");
-    res.redirect("/login");
-};
+// middlewareObj.isloggedin = function (req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return next();
+//     }
+//     req.flash("error", "please log in or sign up to access that page");
+//     res.redirect("/login");
+// };
 
-middlewareObj.isAdmin = (req, res, next) => {
-    if (req.isAuthenticated()) {
-        if (req.user.username == "admin") {
-            return next();
-        } else {
-            req.flash("error", "You don't have access to that page");
-            return res.redirect("/alljobs");
-        }
-    } else {
-        req.flash("error", "please log in or sign up to access that page");
-        return res.redirect("/login");
-    }
-};
+// middlewareObj.isAdmin = (req, res, next) => {
+//     if (req.isAuthenticated()) {
+//         if (req.user.username == "admin") {
+//             return next();
+//         } else {
+//             req.flash("error", "You don't have access to that page");
+//             return res.redirect("/alljobs");
+//         }
+//     } else {
+//         req.flash("error", "please log in or sign up to access that page");
+//         return res.redirect("/login");
+//     }
+// };
 
 module.exports = middlewareObj;
